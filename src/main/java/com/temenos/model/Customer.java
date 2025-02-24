b22b632928a7f7565a70fc0659b940ee5c7cee97
MalekJabri <33630653+MalekJabri@users.noreply.github.com>
2025-02-24 16:50:35 +0100

    Correction file position

---
+++ src/main/java/com/temenos/model/Customer.java
@@ -0,0 +1,20 @@
+package com.temenos.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@ToString
+public class Customer {
+
+    private String name;
+    private String lastName;
+    private String CustomerID;
+    private String phone;
+    private String email;
+
+}
+   
